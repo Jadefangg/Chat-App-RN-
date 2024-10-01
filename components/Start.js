78889030d96791560afb3145f7dda473ad914750
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-10-01 13:31:14 +0530

    5.3

--- components/Start.js
+++ components/Start.js
@@ -1,23 +1,77 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity, ImageBackground, Alert } from 'react-native';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
-const Screen1 = ({ navigation }) => {
+const Start = ({ navigation }) => {
+
+  //state to set name
   const [name, setName] = useState('');
+  //state to set background color
+  const [background, setBackground] = useState('');
+
+  const backgroundImage = require('../img/BackgroundImage.png');
+
+  const auth = getAuth();
+
+  const signInUser = () => {
+      signInAnonymously(auth)
+        .then(result => {
+          navigation.navigate("Chat", {userID: result.user.uid, name: name, background: background });
+          Alert.alert("Signed in Successfully!");
+        })
+        .catch((error) => {
+          Alert.alert("Unable to sign in, try later again.");
+        })
+    }
+
 
   return (
-    <View style={styles.container}>
-      <Text>Hello Screen1!</Text>
+    <View style={styles.container} >
+      <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.image}>
+      <Text>Hello and welcome:{name}</Text>
       <TextInput
         style={styles.textInput}
         value={name}
         onChangeText={setName}
         placeholder='Type your username here'
       />
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+      style={[styles.colorButton, {backgroundColor: '#FF474C'}]}
+      onPress={ ()=> setBackground("#FF474C")}
+      >
+        <Text style={styles.buttonText}>
+          Set Color!
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      style={[styles.colorButton, {backgroundColor: '#ADD8E6'}]}
+      onPress={ ()=> setBackground("#ADD8E6")}
+      >
+        <Text style={styles.buttonText}>
+          Set Color!
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      style={[styles.colorButton, {backgroundColor: '#90EE90'}]}
+      onPress={ ()=> setBackground("#90EE90")}
+      >
+        <Text style={styles.buttonText}>
+          Set Color!
+        </Text>
+      </TouchableOpacity>
+    </View>
       <Button
-        title="Go to Screen 2"
-        onPress={() => navigation.navigate('Screen2', { name: name})}            
-        
+        title="Go to Chat"
+        onPress={() => {
+          if (name == '') {
+            Alert.alert('Type a name');
+          } else {
+            signInUser();
+          }
+          }}
       />
+      </ImageBackground>
     </View>
   );
 }
@@ -25,8 +79,12 @@ const Screen1 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-     justifyContent: 'center',
+    justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flex: 1
   },
   textInput: {
     width: "88%",
@@ -34,7 +92,22 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 15,
     marginBottom: 15
-  }
+  },
+  colorButton: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  buttonText: {
+    padding: 20,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 });
 
-export default Screen1;
+export default Start;
