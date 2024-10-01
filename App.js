80c594552209893b4b8dfda2a82fe6cf8f9310f3
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-10-01 14:14:55 +0530

    app.js improvements and comments

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { getFirestore } from 'firebase/firestore'; // Import the functions you n
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import the screens
-import ShoppingLists from './components/ShoppingLists';
+import Start from './components/Start';
 import Chat from './components/Chat';
 
 // Create the navigator
@@ -14,7 +14,8 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   // Your web app's Firebase configuration
-  const firebaseConfig = {
+  const firebaseConfig = { //this is the configuration for the firebase app and is unique to your app
+    //All of the following should be stored in environment variables for proper practice and security - this is just for demonstration purposes.
     apiKey: "AIzaSyALAciQB_zxI8PgiZw6YM-J0wY5iE0R4z4",
     authDomain: "shopping-list-demo-b3785.firebaseapp.com",
     projectId: "shopping-list-demo-b3785",
@@ -29,17 +30,14 @@ const App = () => {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="Start"
-    >
-      <Stack.Screen name="Start" component={Start} />
-      <Stack.Screen
-        name="Chat"
-      >
-        {props => <Chat db={db} {...props} />}
-      </Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-//export default App;
+
+export default App;
