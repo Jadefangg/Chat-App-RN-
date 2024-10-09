c0b5572c1b83e3ff7bb2a0e199b995d0e9c1925a
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-10-09 20:49:05 +0530

    5.5

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+//import { getFirestore } from 'firebase/firestore';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
 import { useNetInfo }from '@react-native-community/netinfo'; // <<<<< this isn't a regular function it acts as a react hook.
 import { useEffect } from 'react';
-import { LogBox, Alert } from 'react-native';
+import { LogBox, Alert, StyleSheet } from 'react-native';
+import { getStorage } from "firebase/storage";
 import Start from './components/Start';
 import Chat from './components/Chat';
 
@@ -14,7 +15,8 @@ import Chat from './components/Chat';
 const Stack = createNativeStackNavigator();
 
 const App = () => { //NetInfo Implemented with its alert.
-  const connectionStatus = useNetInfo();
+
+  const connectionStatus = useNetInfo(); //this is a hook that returns the network status of the device.
   useEffect(() => {
     if (connectionStatus.isConnected === false) {
       Alert.alert("Connection Lost!");
@@ -34,8 +36,9 @@ const App = () => { //NetInfo Implemented with its alert.
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+  const app = initializeApp(firebaseConfig);//this initializes the firebase app with the firebase configuration.
+  const db = getFirestore(app);//this is the firestore database reference that will be used to read and write data.
+  const storage = getStorage(app); //this is the storage reference that will be used to upload and download images.
 
   //the db variable is passed as a db prop however, the db prop can be passed under a different name - eg. db={database}
   return (
@@ -43,11 +46,18 @@ const App = () => { //NetInfo Implemented with its alert.
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}  
+          {props => <Chat isConnected={connectionStatus.isConnected} db={db} storage={storage} {...props} />}  
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 export default App;
